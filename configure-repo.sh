2c3d0686543ec7c5de99444f9174ebaec8595230
Megh KC <megh.b.kc@usu.edu>
2023-01-25 10:12:16 -0700

    Add files via upload

---
+++ configure-repo.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+git config --local include.path ../.gitconfig
+
+echo Just ran the command
+echo   git config --local include.path ../.gitconfig
+echo 
+echo This ensures that your repository will respect the configuration settings
+echo for this project.
